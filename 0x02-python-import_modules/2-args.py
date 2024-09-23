d1c93af84791839430061a07f1f4610e7a71e2f9
Fatima-Aharchaoy <fatimaaharchaoy1996@gmail.com>
2024-09-23 15:44:21 -0700

    jq

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 import sys
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+def main():
+    argv = sys.argv[1:]  # Exclude the script name from the arguments list
+    arg_count = len(argv)  # Number of arguments
+
+    if arg_count == 0:
+        print(f"{arg_count} arguments.")
+    elif arg_count == 1:
+        print(f"{arg_count} argument:")
+    else:
+        print(f"{arg_count} arguments:")
+
+    # Print each argument with its position
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
+
+if __name__ == "__main__":
+    main()
